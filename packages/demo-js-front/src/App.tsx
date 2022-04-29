4a54e19b2e72d330a85fb743cdec64887146d855
Veralalala <15951908185@163.com>
2022-04-29 14:18:59 +0800

    feat: add open-bid demopage

--- packages/demo-js-front/src/App.tsx
+++ packages/demo-js-front/src/App.tsx
@@ -3,21 +3,36 @@ import "./App.css";
 import {
     fcl,
     FlowEnv,
-    MatrixMarketplaceNFTClient
+    MatrixMarketplaceNFTClient,
+    MatrixMarketplaceOpenBidClient
 } from "@matrix-labs/matrix-marketplace-nft-sdk"
-const client = new MatrixMarketplaceNFTClient();
+const openBidClient = new MatrixMarketplaceOpenBidClient();
+const nftClient = new MatrixMarketplaceNFTClient();
+
+const network = process.env.REACT_APP_NETWORK;
 function App() {
 
     const check = useCallback(async () => {
-        // await client.setupGlobalFcl(FlowEnv.localEmulator);
-        // await client.setupGlobalFcl(FlowEnv.flowMainnet);
-        await client.setupGlobalFcl(FlowEnv.flowTestnet);
-        await fcl.logIn();
-        await fcl.authenticate();
+        await fcl.currentUser.unauthenticate()
+        console.log(`checking.....${network}`)
+        if(network === 'test') {
+            console.log('checking test....')
+            await nftClient.bindFcl(fcl, FlowEnv.flowTestnet);
+            await openBidClient.bindFcl(fcl, FlowEnv.flowTestnet);
+            await fcl.logIn();
+            await fcl.authenticate();
+        } else if (network === 'local'){
+            console.log('checking local....')
+            await nftClient.bindFcl(fcl, FlowEnv.localEmulator);
+            await openBidClient.bindFcl(fcl, FlowEnv.localEmulator);
+            await fcl.logIn();
+            await fcl.authenticate();
+        }
+
     },[]);
 
     useEffect(() => {
-      check();
+        check();
     },[check])
 
 
@@ -26,7 +41,7 @@ function App() {
         let ret;
         const user = await fcl.currentUser().snapshot();
         console.log(user);
-        ret = await client.mintNFTs('0x7f3812b53dd4de20',[user.addr], ["1231as"],  [{key:"version",value:"1.0.0"}]).catch(console.error);
+        ret = await nftClient.mintNFTs('0x7f3812b53dd4de20',[user.addr], ["1231as"],  [{key:"version",value:"1.0.0"}]).catch(console.error);
         console.log(ret);
     };
 
@@ -34,8 +49,8 @@ function App() {
     const checkNFTsCollection = async () => {
         let ret;
         const user = await fcl.currentUser().snapshot();
-        //ret = await client.checkNFTsCollection("0x445697f20309b7c0");
-        ret = await client.checkNFTsCollection(user.addr);
+        //ret = await nftClient.checkNFTsCollection("0x445697f20309b7c0");
+        ret = await nftClient.checkNFTsCollection(user.addr);
         console.log(ret);
     };
 
@@ -45,17 +60,68 @@ function App() {
 
         const user = await fcl.currentUser().snapshot();
         console.log(user);
-        ret = await client.getNFTs(user.addr);
+        ret = await nftClient.getNFTs(user.addr);
         console.log(ret);
     };
 
     const initNFTCollection = async () => {
 
         let ret;
-        ret = await client.initNFTCollection();
+        ret = await nftClient.initNFTCollection();
         console.log(ret);
     };
 
+    const initOpenBid = async () => {
+        let ret;
+        const user = await fcl.currentUser().snapshot();
+        console.log(user);
+        ret = await openBidClient.initOpenBid().catch(console.error);
+        console.log(ret);
+    };
+
+    const openBid = async () => {
+        let ret;
+        const user = await fcl.currentUser().snapshot();
+        console.log(user);
+        ret = await openBidClient.openBid(19, '2.58' ).catch(console.error);
+        console.log(ret);
+    };
+
+    const removeOpenBid = async () => {
+        let ret;
+        const user = await fcl.currentUser().snapshot();
+        console.log(user);
+        ret = await openBidClient.removeBid(90575769 ).catch(console.error);
+        console.log(ret);
+    };
+
+    const acceptBid = async () => {
+        let ret;
+        const user = await fcl.currentUser().snapshot();
+        console.log(user);
+        ret = await openBidClient.acceptBid(90576137, '0xae8b87df71d454cb' ).catch(console.error);
+        console.log(ret);
+    };
+
+    const getBidDetails = async () => {
+
+        let ret;
+
+        const user = await fcl.currentUser().snapshot();
+        console.log(user);
+        ret = await openBidClient.getBidDetails("0xae8b87df71d454cb", 90576137);
+        console.log(ret);
+    };
+
+    const getBidIds = async () => {
+
+        let ret;
+
+        const user = await fcl.currentUser().snapshot();
+        console.log(user);
+        ret = await openBidClient.getBidIds(user.addr);
+        console.log(ret);
+    };
 
   return (
     <div className="App">
@@ -70,6 +136,25 @@ function App() {
       </button>
         <button onClick={mint} className="App-link">
             mint
+        </button>
+
+        <button onClick={initOpenBid} className="App-link">
+            initOpenBid
+        </button>
+        <button onClick={openBid} className="App-link">
+            openBid
+        </button>
+        <button onClick={removeOpenBid} className="App-link">
+            removeOpenBid
+        </button>
+        <button onClick={acceptBid} className="App-link">
+            acceptBid
+        </button>
+        <button onClick={getBidDetails} className="App-link">
+            getBidDetails
+        </button>
+        <button onClick={getBidIds} className="App-link">
+            getBidIds
         </button>
     </div>
   );
